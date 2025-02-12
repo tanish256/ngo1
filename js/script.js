246f72558435f2e8b51be81d7ad53a05a7d7b915
tanish256 <tanishsempala@gmail.com>
2025-02-12 23:44:15 +0300

    alot of images gallery.html

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$('#menul').click(function () {
+    $('.container').toggleClass('active');
+    $('#menuk').slideToggle();
+  });
+  $('.blue').click(function(){
+    window.open('https://www.scec-technologies.com', 'name'); 
+  });
